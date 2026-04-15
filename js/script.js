7d07f3f6354a1a11a39997eb20cf1e8a7a4341da
Anthony <cosah@users.noreply.github.com>
2026-04-15 09:33:18 -0400

    disco button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-const addNumbers = (first, second) => {
-  return first + second;
-}
+const discoButton = document.querySelector('#disco-button');
+discoButton.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomColor();
+});
 
-if (typeof module !== 'undefined') {
-  module.exports = {
-    addNumbers
-  }
+//todo define getRandomColor that selects a random color from a predefined list of colors.
+function getRandomColor() {
+  const colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33A1', '#33FFF6'];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
-
