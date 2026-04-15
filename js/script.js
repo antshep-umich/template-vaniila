a785505347176c2768d30c5e16396a984d1546e8
Anthony <cosah@users.noreply.github.com>
2026-04-15 09:37:11 -0400

    syntax rules

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ discoButton.addEventListener('click', () => {
   document.body.style.backgroundColor = getRandomColor();
 });
 
-//todo define getRandomColor that selects a random color from a predefined list of colors.
-function getRandomColor() {
-  const colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33A1', '#33FFF6'];
-  return colors[Math.floor(Math.random() * colors.length)];
-}
+const colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33A1', '#33FFF6'];
+  
+const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)];
